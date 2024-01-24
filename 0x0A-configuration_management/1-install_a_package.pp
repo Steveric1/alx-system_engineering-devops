29f32018580b85fd39a1945f25a70a9c46371ce8
Eric <stephen.erich243@gmail.com>
2024-01-24 20:54:14 +0100

    task1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,5 @@
-#Using Puppet, install flask from pip3.
-#Requirements:
-#Install flask
-#Version must be 2.1.0
-
-package {'flask':
+# install flask from pip3
+package { 'flask' :
     ensure   => '2.1.0',
-    provider => 'pip3'
+    provider => 'pip3',
 }
