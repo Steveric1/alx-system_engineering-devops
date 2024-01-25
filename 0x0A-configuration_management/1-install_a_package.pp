cb24d73021153067f925e5805dcd7cfc24381982
Eric <stephen.erich243@gmail.com>
2024-01-25 11:41:04 +0100

    flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,11 @@
 # install flask from pip3
 
-package { 'flask' :
-    ensure   => 'installed',
+package { 'Werkzeug' :
+    ensure   => '2.1.1',
     provider => 'pip3',
-    version  => '2.1.0',
+}
+
+package {'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',
 }
