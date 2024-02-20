cccaed1c1be3a0f2bc5637a0d3a1f094cf8aaf24
Eric <stephen.erich243@gmail.com>
2024-02-20 13:06:54 +0100

    csv extraction

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Using what you did in the task #0, extend your Python script to export
+data in the CSV format."""
+
+import json
+import sys
+import urllib.request
+import csv
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/users/"
+    with urllib.request.urlopen(url + sys.argv[1]) as response:
+        data = json.loads(response.read().decode("utf-8"))
+        username = data.get('username')
+    url = "https://jsonplaceholder.typicode.com/todos?userId="
+    with urllib.request.urlopen(url + sys.argv[1]) as response:
+        data = json.loads(response.read().decode("utf-8"))
+        with open(sys.argv[1] + ".csv", mode='w') as file:
+            writer = csv.writer(file, delimiter=',', quotechar='"',
+                                quoting=csv.QUOTE_ALL)
+            [
+                writer.writerow([
+                    sys.argv[1],
+                    username,
+                    task.get('completed'),
+                    task.get('title')
+                ]) for task in data
+            ]
