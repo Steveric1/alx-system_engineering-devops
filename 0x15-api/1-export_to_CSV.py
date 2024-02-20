1ea96530788af92f8b7770c2c36e7d967d2a0926
Eric <stephen.erich243@gmail.com>
2024-02-20 14:07:25 +0100

    csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,26 +1,45 @@
-#!/usr/bin/python3
-"""Using what you did in the task #0, extend your Python script to export
-data in the CSV format."""
-
 import csv
 import json
 import sys
 import urllib.request
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/users/"
-    with urllib.request.urlopen(url + sys.argv[1]) as response:
-        data = json.loads(response.read().decode("utf-8"))
-        username = data.get('username')
-    url = "https://jsonplaceholder.typicode.com/todos?userId="
-    with urllib.request.urlopen(url + sys.argv[1]) as response:
-        data = json.loads(response.read().decode("utf-8"))
-        with open(sys.argv[1] + ".csv", mode='w', newline="") as file:
-            writer = csv.writer(file, quoting=csv.QUOTE_ALL)
-            writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS",
-                             "TASK_TITLE"])
-            task_count = 0
-            for task in data:
-                writer.writerow([sys.argv[1], username, task.get("completed"),
-                                 task.get("title")])
-                task_count += 1
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
+
+    user_id = sys.argv[1]
+
+    try:
+        # Fetch user data
+        u_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
+        with urllib.request.urlopen(u_url) as response:
+            user_data = json.loads(response.read().decode())
+            username = user_data.get('username')
+
+        # Fetch tasks data
+        t_url = f"https://jsonplaceholder.typicode.com/todos?userId={user_id}"
+        with urllib.request.urlopen(t_url) as response:
+            tasks_data = json.loads(response.read().decode())
+
+        # Write data to CSV
+        csv_file = f'{user_id}.csv'
+        fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS',
+                      'TASK_TITLE']
+        with open(csv_file, mode='w', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+            writer.writeheader()
+
+            # Write tasks to CSV
+            for task in tasks_data:
+                writer.writerow({
+                    'USER_ID': user_id,
+                    'USERNAME': username,
+                    'TASK_COMPLETED_STATUS': 'Completed'
+                    if task.get('completed') else 'Incomplete',
+                    'TASK_TITLE': task.get('title')
+                })
+
+        print(f'Data has been exported to {csv_file}')
+    except Exception as e:
+        print("An error occurred:", e)
